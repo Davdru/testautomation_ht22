a6f8996f217f31d5ac1c4e67d4c2f87f98f6298d
bjorn <bjorn@bjornkjellgren.se>
2022-10-25 14:23:05 +0200

    Exempel på start av webläsare och navigation.

--- test_chalmers.py
+++ test_chalmers.py
@@ -6,13 +6,14 @@ from selenium.webdriver.common.by import By
 # 1 målet
 # Öppna en webläsare som vi har kontroll över och gå till www.chalmers.se
 
+def test_nav_it():
+    browser = webdriver.Edge() # Starta en webläsare och ge mig en referens till den så att jag kan styra den
+    browser.set_window_size(1920, 1080)
+    browser.get("https://www.chalmers.se")
+    browser.find_element(By.LINK_TEXT, "Utbildning").click()
+    browser.find_element(By.LINK_TEXT, "Program på grundnivå").click()
+    browser.find_element(By.LINK_TEXT, "Elektro, Data, IT och Medicinteknik").click()
+    browser.find_element(By.CSS_SELECTOR, ".orange li:nth-child(5) .title").click()
 
-browser = webdriver.Edge() # Starta en webläsare och ge mig en referens till den så att jag kan styra den
-browser.set_window_size(1920, 1080)
-browser.get("https://www.chalmers.se")
-browser.find_element(By.LINK_TEXT, "Utbildning").click()
-browser.find_element(By.LINK_TEXT, "Program på grundnivå").click()
-browser.find_element(By.LINK_TEXT, "Elektro, Data, IT och Medicinteknik").click()
-browser.find_element(By.CSS_SELECTOR, ".orange li:nth-child(5) .title").click()
 
-time.sleep(10)
+

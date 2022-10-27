e02967ac3ed17382e040ceb884dee49d19f4f702
bjorn <bjorn@bjornkjellgren.se>
2022-10-27 15:53:37 +0200

    Exempel på användning av propertys för att exponera element i ett page-objekt.

--- test_chalmers_po.py
+++ test_chalmers_po.py
@@ -22,7 +22,7 @@ class ChalmersPage:
         self.driver = driver
 
     def click_utbildning(self) -> ChalmersUtbildning:
-        self.driver.find_element(By.LINK_TEXT, "Utbildning").click()
+        self.utbildning_link.click()
         return ChalmersUtbildning(self.driver)
 
     def click_forskning(self):
@@ -30,6 +30,10 @@ class ChalmersPage:
 
     def click_samverkan(self):
         self.driver.find_element(By.LINK_TEXT, "Samverkan").click()
+
+    @property
+    def utbildning_link(self):
+        return self.driver.find_element(By.LINK_TEXT, "Utbildning")
 
 
 # Den här klassen skall representera huvudsidan chalmers.se med sina olika länkar och innehåll
@@ -41,12 +45,19 @@ class ChalmersUtbildning(ChalmersPage):
     def click_program_grundniva(self):
         self.driver.find_element(By.LINK_TEXT, "Program på grundnivå").click()
 
+    @property
+    def title(self) -> str:
+        return self.driver.find_element(By.XPATH, '//*[@id="ctl00_MSO_ContentDiv"]/div[2]/div[2]/div/div[1]/div/div[1]/div/div/div[2]/h3').text
+
+    @property
+    def body(self) -> str:
+        return self.driver.find_element(By.XPATH, '//*[@id="ctl00_MSO_ContentDiv"]/div[2]/div[2]/div/div[1]/div/div[1]/div/div/div[2]').text
+
 
 def test_nav_till_program_grundniva(browser):
     browser.get("https://www.chalmers.se")
     main_page = ChalmersMainPage(browser)
     utbildning = main_page.click_utbildning()
-
     utbildning.click_program_grundniva()
     # assert browser.current_url == "https://www.chalmers.se/sv/utbildning/Sidor/default.aspx"
 

4f7adf9c8d225f95b600cf0288b79b7bf29d7267
bjorn <bjorn@bjornkjellgren.se>
2022-10-27 13:24:59 +0200

    Förbättrade page-object med arvsstruktur
    Fixtur för att öppna och stänga webbläsare

--- test_chalmers_po.py
+++ test_chalmers_po.py
@@ -1,18 +1,29 @@
+from __future__ import annotations
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-# Den här klassen skall representera huvudsidan chalmers.se med sina olika länkar och innehåll
-class ChalmersMainPage:
+@pytest.fixture
+def browser():
+    driver = webdriver.Edge()
+    driver.set_window_position(0,0)
+    driver.set_window_size(1920, 1080)
+    yield driver
+    driver.quit()
+
+
+class ChalmersPage:
     driver: webdriver.Edge # Vi behöver en driver för att interagera med webbsidan, vi börjar skriva koden för en typ av driver,
     # i mitt fall Edge, vi skall senare ändra så att objektet kan hantera alla olika drivers
 
     def __init__(self, driver: webdriver.Edge):
         self.driver = driver
 
-    def click_utbildning(self):
+    def click_utbildning(self) -> ChalmersUtbildning:
         self.driver.find_element(By.LINK_TEXT, "Utbildning").click()
+        return ChalmersUtbildning(self.driver)
 
     def click_forskning(self):
         self.driver.find_element(By.LINK_TEXT, "Forskning").click()
@@ -20,6 +31,24 @@ class ChalmersMainPage:
     def click_samverkan(self):
         self.driver.find_element(By.LINK_TEXT, "Samverkan").click()
 
+
+# Den här klassen skall representera huvudsidan chalmers.se med sina olika länkar och innehåll
+class ChalmersMainPage(ChalmersPage):
+    pass
+
+
+class ChalmersUtbildning(ChalmersPage):
+    def click_program_grundniva(self):
+        self.driver.find_element(By.LINK_TEXT, "Program på grundnivå").click()
+
+
+def test_nav_till_program_grundniva(browser):
+    browser.get("https://www.chalmers.se")
+    main_page = ChalmersMainPage(browser)
+    utbildning = main_page.click_utbildning()
+
+    utbildning.click_program_grundniva()
+    # assert browser.current_url == "https://www.chalmers.se/sv/utbildning/Sidor/default.aspx"
 
 
 def test_nav_till_utbildning():

747c6d5fc516c41e70805310652fc6a2a63cab76
bjorn <bjorn@bjornkjellgren.se>
2022-11-08 13:26:26 +0100

    Enkel implementation av web-test och fixtur för browser

--- features/environment.py
+++ features/environment.py
@@ -13,3 +13,19 @@ def edge_browser(context) -> webdriver.Edge:
 def before_scenario(context, scenario):
     if "web" in scenario.tags:
         use_fixture(edge_browser, context)
+
+
+
+# Hooks, funktioner vi definierar, verktyget (behave) kör dom vid rätt tidpunkt
+# before_feature(context, feature)
+# before_scenario(context, scenario)
+# before_step(context, step)
+# before_tag(context, tag)
+# before_all(context) # körs först av allt
+
+# after_all(context)
+# after_tag(context, tag)
+# after_step(context, step)
+# after_scenario(context, scenario)
+# after_feature(context, feature)
+
